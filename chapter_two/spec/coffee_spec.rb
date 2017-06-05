b9e17b7550f58e1fe9d31f006544b0e40a476db4
Samseppiol <j_hage@live.com>
2017-06-05 11:30:41 +1000

    Went through writing a description first or writing pending and still running the tests, it will display pending within the terminal

--- chapter_two/spec/coffee_spec.rb
+++ chapter_two/spec/coffee_spec.rb
@@ -6,11 +6,21 @@ end
 RSpec.describe 'A cup of coffee' do
   let(:coffee) {Coffee.new}
 
+  it 'is light in color' do
+    pending 'Color not implemented yet'
+    expect(coffee.color).to be(:light)
+  end
+
+  it 'is cooler than 200 degrees Fahrenheit' do
+    pending 'Temperature not implemented yet'
+    expect(coffee.temperature).to be < 200.0
+  end
+
   it 'costs $1' do
     expect(coffee.price).to eq(1.00)
   end
 
-  fcontext 'with milk' do
+  context 'with milk' do
     before {coffee.add :milk}
 
     it 'costs $1.25' do
